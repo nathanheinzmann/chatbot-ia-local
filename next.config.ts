3c9768ba9270851291d7a34398dcbddecc9b4b3a
Nathan Heinzmann <nathan.heinzmann@newfold.com>
2025-06-18 13:21:14 -0300

    fix: remove experimental.serverActions and turbo config to fix build error

--- next.config.ts
+++ next.config.ts
@@ -10,18 +10,6 @@ const nextConfig: NextConfig = {
     };
     return config;
   },
-  experimental: {
-    serverActions: true,
-  },
-  // Disable Turbopack for now as it has issues with Transformers.js
-  turbo: {
-    rules: {
-      // Disable Turbopack for this package
-      '@xenova/transformers': {
-        loader: 'webpack',
-      },
-    },
-  },
 };
 
 export default nextConfig;
